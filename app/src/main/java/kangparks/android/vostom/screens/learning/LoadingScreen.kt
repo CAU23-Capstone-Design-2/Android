266677ac75e5f541f0472d1440c314659956d72e
pjw-redt <pjiww4@gmail.com>
2023-10-06 18:27:16 +0900

    LoadingScreen

--- app/src/main/java/kangparks/android/vostom/screens/learning/LoadingScreen.kt
+++ app/src/main/java/kangparks/android/vostom/screens/learning/LoadingScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import kangparks.android.vostom.Greeting
 import kangparks.android.vostom.components.buttonsheet.BottomSheet
+import kangparks.android.vostom.components.template.LearningLayoutTemplate
+import kangparks.android.vostom.navigations.Content
 
 @Composable
 fun LoadingScreen(navController : NavHostController){
@@ -18,8 +20,20 @@ fun LoadingScreen(navController : NavHostController){
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Greeting("학습 후 로딩 화면")
+        LearningLayoutTemplate(
+            hasMainContent = true,
+            mainContent = "Vostom에서 사용자의 목소리를\n열심히 학습하고 있어요",
+            hasSubContent = true,
+            subContent = "작업이 완료되면 알려드릴게요!",
+            hasNextButton = false,
+            nextButtonAction = {
+                navController.navigate(Content.LearningPitch.route)
+            },
+            nextButtonBottomPaddingValue = 60
+        ) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Greeting("로티에서 로딩중 에니메이션 찾아서 넣기!")
+            }
         }
         BottomSheet()
     }
